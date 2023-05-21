fd0ac319f029304757cbd115bf3be0859d3931c9
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-05-21 12:26:20 +0100

    Implemented basic site layout

--- addon-templatinator/src/App.vue
+++ addon-templatinator/src/App.vue
@@ -1,7 +1,27 @@
 <template>
-  <router-view></router-view>
+  <article class="site-wrapper">
+    <banner-vue/>
+    <router-view></router-view>
+    <footer-vue/>
+  </article>
 </template>
 
-<style lang="scss">
+<script lang="ts">
+import BannerVue from '@/components/BannerVue.vue'
+import FooterVue from './components/FooterVue.vue';
 
+export default {
+  components: {
+    BannerVue,
+    FooterVue
+  }
+}
+</script>
+
+<style lang="scss">
+.site-wrapper {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
 </style>
