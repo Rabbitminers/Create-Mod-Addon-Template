6f787c62e5ead396a479cf8c19097ee163244543
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-05-21 12:26:55 +0100

    Implemented form section wrapper

---
+++ addon-templatinator/src/components/FormSection.vue
@@ -0,0 +1,54 @@
+<template>
+    <li :key="title" class="event" :data-date="timeline">
+        <div class="member-infos">
+        <h1 class="member-title" @click="expand()">{{ title }}</h1>
+        
+        <ul class="member-contact">
+            <li v-if="icon" class="member-website">
+                <a :class="icon" target="_blank" :href="icon"></a>
+            </li>
+        </ul>
+
+        
+        <div class="member-parameters" :class="{ open: open }">
+            <br/>
+            <slot></slot>
+        </div>
+        </div>
+    </li>
+</template>
+
+<script lang="ts">
+export default {
+    name: "FormSection",
+    props: {
+        title: {
+            type: String,
+            default: "Invalid title"
+        },
+        timeline: {
+            type: String,
+            default: ""
+        },
+        icon: {
+            type: String,
+            default: "entypo-globe"
+        },
+        open_by_default: {
+            type: Boolean,
+            default: false,
+        }
+    },
+    data() {
+        return {
+            open: this.open_by_default
+        }
+    },
+    methods: {
+        expand: function (): void {
+            this.open = !this.open;
+        }
+    }
+}
+</script>
+
