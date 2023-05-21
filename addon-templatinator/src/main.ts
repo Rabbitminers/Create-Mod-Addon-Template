0defa86f10f89001ab3026c65c3e27f104c4767a
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-05-21 12:26:34 +0100

    Imported stylesheets at project root

--- addon-templatinator/src/main.ts
+++ addon-templatinator/src/main.ts
@@ -2,4 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import './theme/global.scss'
+import './theme/theme.scss'
+
 createApp(App).use(router).mount('#app')
